4b812308476a806ad0a7c2dcd0ce6f28f199e059
Rafee Jenkins <rafeejenkins@gmail.com>
2025-01-08 19:12:05 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
-# Check if Git is installed
-git status
-# Install OhMyZsh
+#!/bin/bash
+
+# Set up Oh My Zsh
+echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Homebrew
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+
+# Install Powerlevel10k theme
+echo "Installing Powerlevel10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install Zsh plugins
+echo "Installing Zsh plugins..."
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Clone dotfiles repository and move configuration files
+echo "Cloning dotfiles and setting up configurations..."
+mkdir -p ~/dev
+cd ~/dev
+git clone https://github.com/rafeeJ/dots.git
+cd ..
+mv ./dev/dots/.p10k.zsh ~/.p10k.zsh
+
+# Source Zsh configuration
+echo "Sourcing .zshrc..."
+source ~/.zshrc
+
+echo "Setup complete! Restart your terminal or source your .zshrc again to apply the changes."
